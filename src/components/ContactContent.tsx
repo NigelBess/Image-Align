1446297001df8d071873698234cc4027ea17b7d5
NigelBess <nigelbess@gmail.com>
2024-05-03 12:21:08 -0700

    created contact section

--- src/components/ContactContent.tsx
+++ src/components/ContactContent.tsx
@@ -1,5 +1,8 @@
 export const ContactContent: React.FC = () => {
     return(
-        <div>nigelbess@gmail.com</div>
+        <div className="Contact CenterStackPanel"><div>Found a problem?</div><div>Got a feature request?</div> <div> Shoot us an email at</div>
+        <a href="mailto:imageAlign.io@gmail.com" className="ContactEmail">imageAlign.io@gmail.com</a>
+        <div>We listen!</div>
+        </div>
     )
 }
